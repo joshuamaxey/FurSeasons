9513535b13a324da9be897ab261828225b8bddf5
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-11-23 14:02:13 -0600

    finished ProfileButton component including drop-down menu

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
@@ -6,18 +6,41 @@ import styles from './navigation.module.css'
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // This keeps our click from bubbling up to the document and triggering 'closeMenu'
+        setShowMenu(!showMenu); // if (!showMenu), setShowMenu(true);
+    }
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
 
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout());
     };
 
+    const ulClassName = `${styles.profileDropdown} ${showMenu ? "" : styles.hidden}`;
+
     return (
         <>
-            <button className={styles.profileButton}>
+            <button className={styles.profileButton} onClick={toggleMenu}>
                 <FaUserCircle />
             </button>
-            <ul className={styles.profileDropdown}>
+            <ul ref={ulRef} className={ulClassName}>
                 <li>{user.username}</li>
                 <li>{user.firstName} {user.lastName}</li>
                 <li>{user.email}</li>
